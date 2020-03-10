3142f0181e616688f83462bf6f3de854f3f73010
sinseiki <eekdland@naver.com>
2020-03-10 17:28:34 +0900

    15.3. state를 이용해서 여러개의 엘리먼트를 생성하기 (TypeScript)

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,16 @@ import Subject from "./components/Subject"
 import './App.css';
 
 
-class App extends Component<{},{subject:any}> {
+class App extends Component<{},{subject:any,contents:Array<any>}> {
   constructor(props:JSX.Element){
     super(props);
     this.state = {
-      subject:{title:'WEB', sub:'World Wid Web!'}
+      subject:{title:'WEB', sub:'World Wid Web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
     }
   }
   render() {
@@ -17,8 +22,9 @@ class App extends Component<{},{subject:any}> {
     <div className="App">
       <Subject 
         title={this.state.subject.title} 
-        sub={this.state.subject.sub}></Subject>
-      <TOC></TOC>
+        sub={this.state.subject.sub}>
+        </Subject>
+        <TOC data={this.state.contents}></TOC>
       <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
     </div>
     );
